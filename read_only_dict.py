1eb2c4343cd73abdd7afbf999d10ae45402737ae
Ankit Yadav <42907602+masterPiece93@users.noreply.github.com>
2022-06-30 10:44:07 +0530

    Create read_only_dict.py
    added basic implementation of read only dict in python

---
+++ read_only_dict.py
@@ -0,0 +1,15 @@
+import collections
+
+
+class DictWrapper(collections.abc.Mapping):
+    def __init__(self, data):
+        self._data = data
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+    def __len__(self):
+        return len(self._data)
+
+    def __iter__(self):
+        return iter(self._data)
